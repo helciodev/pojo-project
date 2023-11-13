55c3dbcc5151fa7c91c4fb3febdc5fd2236d3fce
helcio <h.andre@giap.pl>
2023-11-13 21:18:09 +0100

    Fix problem 13

--- problems/13-string-converter.js
+++ problems/13-string-converter.js
@@ -12,8 +12,19 @@ console.log(stringConverter("raccoon")); // => {r: 1, a: 1, c: 2, o: 2, n: 1}
 ***********************************************************************/
 
 function stringConverter(string) {
-  // Your code here
-}
+  const obj = {};
+  for (let char of string) {
+    if (!obj[char]) {
+      obj[char] = 1;
+    } else {
+      obj[char] += 1;
+    }
+  }
 
+  return obj;
+}
+console.log(stringConverter("apple")); // => {a: 1, p: 2, l: 1, e: 1}
+console.log(stringConverter("banana")); // => {b: 1, a: 3, n: 2}
+console.log(stringConverter("raccoon")); // => {r: 1, a: 1, c: 2, o: 2, n: 1}
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = stringConverter;
